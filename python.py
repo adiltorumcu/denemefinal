b8157d6df12f63b97231224853b67f742ab510ff
eneskartal1 <kartalenes1001@gmail.com>
2022-05-16 15:19:05 +0300

    başlangıç2

--- python.py
+++ python.py
@@ -2,3 +2,9 @@
 Example code showing how to create a button,
 and the three ways to process button events.
 """
+import arcade
+import arcade.gui
+
+
+#--- Method 1 for handling click events,
+# Create a child class.
