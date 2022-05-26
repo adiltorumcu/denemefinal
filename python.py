4d6a743a6d22f073ef7e746e398365f4152aeb5b
adiltorumcu <100956073+adiltorumcu@users.noreply.github.com>
2022-05-26 12:47:51 +0300

    ekran başlangıç

--- python.py
+++ python.py
@@ -1,3 +1,15 @@
-print("hello world")
-for i in range(100):
-            print("hello world")
+"""
+Example code showing how to create a button,
+and the three ways to process button events,
+"""
+
+import arcade
+import arcade.gui
+
+
+#--- Method 1 for handling click events,
+# Create a child class.
+
+class QuitButton(arcade.gui.UIFlatButton) : 
+      def  on_click(self, event: arcade.gui.UIOnChangeEvent) :
+        arcade. exit()
