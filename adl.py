3e29eb523407676bc0c7bda0998e9d53f74df58a
adiltorumcu <100956073+adiltorumcu@users.noreply.github.com>
2022-05-27 17:47:42 +0300

    menü başlangıç

--- adl.py
+++ adl.py
@@ -1,1 +1,12 @@
+"""
+Example code showing how to create a button,
+and the three ways to process button events.
+"""
+import arcade
+import arcade.gui
 
+# --- Method 1 for handling click events,
+# Create a child class.
+class QuitButton(arcade.gui.UIFlatButton) :
+    def on_click(self, event: arcade.gui.UIOnClickEvent):
+        arcade.exit()
