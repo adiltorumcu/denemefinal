e443e316f00276f7304b8792a6446ce22652b695
adiltorumcu <100956073+adiltorumcu@users.noreply.github.com>
2022-05-30 10:27:18 +0300

    hata düzeltme

--- adl.py
+++ adl.py
@@ -14,24 +14,24 @@ class QuitButton(arcade.gui.UIFlatButton) :
 
 
 class MyWindow(arcade.Window):
-    def__init__(self):
-    super().__init__(800,600,"UIFlatButton Example", resizable=True)
+    def __init__(self):
+        super().__init__(800,600, "UIFlatBUtton,",resizable=True)
 
-    # ---Reuquired for all code that uses UI element,
-    # a UIManager to handle the UI.
-    self.manager = arcade.gui.UIManager()
-    self.manager.enable()
+        # ---Reuquired for all code that uses UI element,
+        # a UIManager to handle the UI.
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
 
-    # Set background color
-    arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+        # Set background color
+        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
 
-    # Create a vertical BoxGroup to align buttons
-    self.v_box = arcade.gui.UIBoxLayout()
+        # Create a vertical BoxGroup to align buttons
+        self.v_box = arcade.gui.UIBoxLayout()
 
-    # Create the buttons.
-    start_button = arcade.gui.UIFlatButton(text= "Start Game ", widht=200)
-    self.v_box.add(start_button.with_space_around(bottom=20))
+        # Create the buttons.
+        start_button = arcade.gui.UIFlatButton(text= "Start Game ", widht=200)
+        self.v_box.add(start_button.with_space_around(bottom=20))
 
-    settings_button = arcade.gui.UIFlatButton(text="Settings",widht=200)
-    self.v_box.add(settings_button.with_space_around(bottom=20))
+        settings_button = arcade.gui.UIFlatButton(text="Settings",widht=200)
+        self.v_box.add(settings_button.with_space_around(bottom=20))
     
