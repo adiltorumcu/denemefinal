aba8e3bb5c2473766785c23e328b6b4b3faac0eb
eneskartal1 <kartalenes1001@gmail.com>
2022-06-15 11:23:27 +0300

    Update adl.py

--- adl.py
+++ adl.py
@@ -14,10 +14,9 @@ class QuitButton(arcade.gui.UIFlatButton) :
          arcade. exit() 
 
 class MyWindow(arcade.Window):
-<<<<<<< HEAD
-=======
 
->>>>>>> d7d190a22ac76a418aa8d578e1afa8b5ccd6fad9
+
+
     def __init__(self):         
             super().__init__(800,600, "UIFlatBUtton Example,",resizable=True)
 
@@ -61,63 +60,8 @@ class MyWindow(arcade.Window):
                 child=self.v_box)
         ) 
 
-<<<<<<< HEAD
-def on_draw(self):
-            self.clear()
-            self.manager.draw()
 
-=======
-    def __init__(self):
-        super().__init__(800, 600, "UIFlatButton Example" , resizable=True)
 
-        # --- Required for all code that uses UI element,
-        # a UIManager to handle the UI.
-        self.manager = arcade.gui.UIManager()
-        self.manager.enable()
-
-        # Set background color 
-        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
-
-        # Create a vertical BoxGroup to align buttons
-        self.v_box = arcade.gui.UIBoxLayout()
-    
-        # Create the buttons 
-        start_button = arcade.gui.UIFlatButton(text="Quit",width=200)
-        self.v_box.add(start_button.with_space_around(bottom=20))
-
-        settings_button =arcade.gui.UIFlatButton(text="Settings" , width=200)
-        self.v_box.add(settings_button.with_space_around(bottom=20))
-
-        #Again,method 1. Use child class to handle events.
-        quit_button = QuitButton(text="Start Game", width=200)
-        self.v_box.add(quit_button)
-
-        # --- Method 2 for handling click events,
-        # assing self.on_click_start as callback
-        start_button.on_click = self.on_click_start
-    
-        # --- Method 3 for handling click events,
-        # use a decorator to handle on_click events
-        @settings_button.event("on_click")
-        def on_click_settings(event):
-            print("Settings:", event)
-    
-        #Create a widget to hold the v_box widget, that will center the buttons
-        self.manager.add(
-            arcade.gui.UIAnchorWidget(
-                anchor_x="center_x",
-                anchor_y="center_y",
-                child=self.v_box)
-        )
-
-    def on_click_start(self,event):
-        print("Start:", event)
-
-    def on_draw(self):
-        self.clear()
-        self.manager.draw()
-
->>>>>>> d7d190a22ac76a418aa8d578e1afa8b5ccd6fad9
 
 window = MyWindow()
 arcade.run()
@@ -238,3 +182,4 @@ while True:
         if hedef.ycor() < -270 or hedef.distance(oyuncu) < 20:
             yaz.write('Maalesef, kaybettiniz!', align='center', font=('Courier', 24, 'bold'))
     if len(hedefler) == 0:
+        yaz.write('Tebrikler, kazandınız!', align='center', font=('Courier', 24, 'bold'))
