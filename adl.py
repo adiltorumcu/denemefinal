fa36d8a450e204ffea9049681f1c4eaac0d52a17
adiltorumcu <100956073+adiltorumcu@users.noreply.github.com>
2022-06-17 10:39:07 +0300

    replay

--- adl.py
+++ adl.py
@@ -222,4 +222,28 @@ while True:
 
 # stores the width of the
 # screen into a variable
-    width = screen.get_width() 
+    width = screen.get_width()
+# stores the height of the
+# screen into a variable
+    height = screen.get_height()
+
+# defining a font
+    smallfont = pygame.font.SysFont('Corbel',35)
+
+# rendering a text written in
+# this font
+    text = smallfont.render('quit' , True , color)
+    while True:
+
+        for ev in pygame.event.get():
+
+            if ev.type == pygame.QUIT:
+             pygame.quit()
+
+        #checks if a mouse is clicked
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+
+        #if the mouse is clicked on the
+        # button the game is terminated
+         if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+             pygame.quit()
